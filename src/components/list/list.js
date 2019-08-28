f79574270cf584dc2149ba3b5e1cd33907230b5c
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-28 15:58:34 +0300

    feat: add buttons to list items

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,15 +1,31 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const uniqid = require('uniqid');
 
 class List extends React.Component {
   createListItem = (props) => (
     <>
-      {props.ListItems.map((item, index) => (
-        <ListGroup.Item key={index}>
-          {item.title}
+      {props.ListItems.map((item) => (
+        <ListGroup.Item key={uniqid()}>
+          <Row>
+            <Col xs={8} md={8} lg={9}>
+              {item.title}
+            </Col>
+            <Col xs={1}>
+              <Button variant="light"><FontAwesomeIcon icon={faTrash} /></Button>
+            </Col>
+            <Col xs={1}>
+              <Button variant="light"><FontAwesomeIcon icon={faCheckCircle} /></Button>
+            </Col>
+          </Row>
         </ListGroup.Item>
       ))}
     </>
